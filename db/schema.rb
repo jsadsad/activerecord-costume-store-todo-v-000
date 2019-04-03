845bb77e18bc011872d73fa1ee0a5f4fd4160867
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-04-03 20:29:02 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.float "price"
     t.string "size"
     t.string "image_url"
     t.datetime "created_at", null: false
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.string "location"
     t.string "theme"
-    t.integer "price"
+    t.float "price"
     t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
